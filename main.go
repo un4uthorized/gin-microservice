26e715b8d9f60a583bc5ba48f7a9379dc5bfcb1e
devneto <miguel.neto320@gmail.com>
2023-01-08 14:04:35 -0300

    feat: add route to return specific album

--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main 
 
 import (
-	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"	
 )
 
 type album struct {
-	ID int `json:"id"`
+	ID string `json:"id"`
 	Title string `json:"title"`
 	Artist string `json:"artist"`
 	Price float64 `json:"price"`
 }
 
 var albums = []album{
-	{ ID: 1, Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ ID: 2, Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ ID: 3, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func getAlbums(ctx *gin.Context) {
@@ -34,9 +33,23 @@ func postAlbums(ctx *gin.Context) {
 	ctx.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+func getAlbumByID(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			ctx.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 }
